bdf39f8c10f502dce8ec0851a597f70b6003c768
Lucas Ferraz <lucas_nogueira155@hotmail.com>
2021-10-04 18:26:18 -0300

    created repository,resource,service and inserted first endpoint

---
+++ src/main/java/com/lucasFerraz/client/resources/ClientResource.java
@@ -0,0 +1,29 @@
+package com.lucasFerraz.client.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasFerraz.client.entities.Client;
+import com.lucasFerraz.client.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@Autowired
+	private ClientService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll(){
+		List<Client> list = service.findAll();
+		return ResponseEntity.ok(list);
+		
+	}
+	
+
+}
---
+++ src/main/java/com/lucasFerraz/client/services/ClientService.java
@@ -0,0 +1,23 @@
+package com.lucasFerraz.client.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lucasFerraz.client.entities.Client;
+import com.lucasFerraz.client.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Client> findAll(){
+		return repository.findAll();
+		
+	}
+}
